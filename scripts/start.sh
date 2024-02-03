d432a6dcb9c924dc3ec6710cb48d91b0916db1a7
Crazykwak <kwj1830@naver.com>
2024-02-03 23:16:37 +0900

    feat : prod env 추가 및 실행 옵션에 프로필 추가

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,7 @@
 PROJECT_ROOT="/home/ubuntu/app"
 JAR_FILE_NAME="back-0.0.1-SNAPSHOT.jar"
 JAR_FILE="$PROJECT_ROOT/back-0.0.1-SNAPSHOT.jar"
+ARG_OPTION="--spring.profiles.active=prod"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
@@ -15,7 +16,7 @@ cp $PROJECT_ROOT/build/libs/$JAR_FILE_NAME $JAR_FILE
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
 source ~/.bashrc
-nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+nohup java -jar $JAR_FILE $ARG_OPTION > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
